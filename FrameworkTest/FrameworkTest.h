75fddb336a8f317004be1220b7a375afba1d70bb
MrFung <2578033436@qq.com>
2020-04-29 01:12:59 +0800

    first commit

---
+++ FrameworkTest/FrameworkTest.h
@@ -0,0 +1,20 @@
+//
+//  FrameworkTest.h
+//  FrameworkTest
+//
+//  Created by 郭枫 on 2020/4/28.
+//  Copyright © 2020 郭枫. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MrfungTest.h"
+
+//! Project version number for FrameworkTest.
+FOUNDATION_EXPORT double FrameworkTestVersionNumber;
+
+//! Project version string for FrameworkTest.
+FOUNDATION_EXPORT const unsigned char FrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTest/PublicHeader.h>
+
+
---
+++ FrameworkTest/MrfungTest.h
@@ -0,0 +1,19 @@
+//
+//  MrfungTest.h
+//  FrameworkTest
+//
+//  Created by 郭枫 on 2020/4/28.
+//  Copyright © 2020 郭枫. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MrfungTest : NSObject
+
+- (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
