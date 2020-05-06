71c53fc3cf70283f57b02f48512dbeca6e43cb0e
MrFung <2578033436@qq.com>
2020-05-07 00:35:21 +0800

    add carthage support

---
+++ Carthage/Build/iOS/MrFungFrameworkTest.framework/Headers/MrFungFramework.h
@@ -0,0 +1,19 @@
+//
+//  MrFungFramework.h
+//  MrFungFrameworkTest
+//
+//  Created by 郭枫 on 2020/5/6.
+//  Copyright © 2020 郭枫. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MrFungFramework : NSObject
+
+- (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Carthage/Build/iOS/MrFungFrameworkTest.framework/Headers/MrFungFrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  MrFungFrameworkTest.h
+//  MrFungFrameworkTest
+//
+//  Created by 郭枫 on 2020/4/29.
+//  Copyright © 2020 郭枫. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MrFungFrameworkTest.
+FOUNDATION_EXPORT double MrFungFrameworkTestVersionNumber;
+
+//! Project version string for MrFungFrameworkTest.
+FOUNDATION_EXPORT const unsigned char MrFungFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MrFungFrameworkTest/PublicHeader.h>
+
+
